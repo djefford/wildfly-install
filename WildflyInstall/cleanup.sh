e3e15a67297cbdc829a44aa946aec40f9c8c7ad6
Jmopper58 <djefford58@gmail.com>
2017-01-26 21:06:02 -0500

    Added all removal pieces.

--- WildflyInstall/cleanup.sh
+++ WildflyInstall/cleanup.sh
@@ -10,3 +10,37 @@
 source ./utilities
 
 # check for and remove Wildfly Home
+wildfly_home=$(ls ${SOFTWARE_HOME} | grep -o "wildfly-[0-9]\.[0-9]\..*")
+wildfly_home=${SOFTWARE_HOME}/${wildfly_home}
+
+if [ -d ${wildfly_home} ]; then
+	printf " %s\n" "Removing ${wildfly_home}."
+	rm -rf ${wildfly_home}
+else
+	printf " %s\n" "Unable to locate ${wildfly_home}."
+fi
+
+# Check for and remove logs directory
+if [ -d ${LOGS_DIR} ]; then
+	printf " %s\n" "Removing ${LOGS_DIR}."
+	rm -rf ${LOGS_DIR}
+else
+	printf " %s\n" "Unable to locate ${LOGS_DIR}."
+fi
+
+# Check for and remove working directories
+if [ -d ./working ]; then
+	printf " %s\n" "Removing ./working."
+	rm -rf ./working
+else
+	printf "%s\n" "Unable to locate ./working."
+fi
+
+if [ -d ./working_scripts ]; then
+	printf " %s\n" "Removing ./working_scripts."
+	rm -rf ./working_scripts
+else
+	printf " %s\n" "Unable to locate ./working_scripts."
+fi
+
+
