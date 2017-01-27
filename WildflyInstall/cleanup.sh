4e0ea4a0bb93c9ec53f36dc735ed496768bfb69c
Jmopper58 <djefford58@gmail.com>
2017-01-26 20:50:56 -0500

    Initialized cleanup.sh script

---
+++ WildflyInstall/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+############################################################
+#
+# A cleanup script to remove directories and scripts from 
+#	the installation of wildfly.
+#
+############################################################
+
+source ./utilities
+
+# check for and remove Wildfly Home
